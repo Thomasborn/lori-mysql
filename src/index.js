498f3e9e5265d2ba4bd346aeb7d64ea3347e518f
Thomas <thomasbtbx@gmail.com>
2024-08-13 01:32:41 +0700

    fix db

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ const  app = express();
 app.use(cookieParser());
 app.use(express.static(publicDirectory));
 app.use(express.json())
-app.set('trust proxy', 1); // Trust the first proxy in the chain (if behind a proxy like Nginx)
+// app.set('trust proxy', 1); // Trust the first proxy in the chain (if behind a proxy like Nginx)
 
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
