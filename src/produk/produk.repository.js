c16c48159b52ae74951bdfa449991b51aaf0e68e
Thomas <thomasbtbx@gmail.com>
2024-11-19 18:14:52 +0700

    fix for FE

--- src/produk/produk.repository.js
+++ src/produk/produk.repository.js
@@ -426,7 +426,7 @@ const newProdukOutlets = await prisma.produk_outlet.createMany({
   data: insert_model.createdVarian.map(variant => ({
     produk_id: variant.createdVariant.id, // Replace with the actual product ID
     outlet_id: 11,                        // Replace with the actual outlet ID if necessary
-    jumlah: data.varian.find(v => v.ukuran === variant.createdVariant.ukuran).stok, // Get the correct stock quantity based on ukuran
+    jumlah: parseInt(data.varian.find(v => v.ukuran === variant.createdVariant.ukuran).stok, 10), // Get the correct stock quantity based on ukuran
   })),
 });
     // Shape the response data to match the desired structure
